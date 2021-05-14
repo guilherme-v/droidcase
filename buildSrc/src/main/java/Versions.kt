45addc4cd1f06dbca757279c9e3a53aa6b66c011
guilherme <silva.guilherme09@gmail.com>
2021-05-13 22:32:38 -0300

    chore: Update Kotlin/Compose versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     // I tried to use kotlin 1.5.0, but Compose seems not work properly with it yet
     // Check presentation->build.gradle.kts "freeCompilerArgs" section to see the error
     // const val kotlin_version = "1.5.0"
-    const val kotlin_version = "1.4.30"
+    const val kotlin_version = "1.4.32"
 
     const val retrofit_version = "2.9.0"
     const val gson_version = "2.8.6"
@@ -10,10 +10,12 @@ object Versions {
     const val room_version = "2.3.0"
     const val kotlin_reflect_version = "1.5.0"
 
+    // https://developer.android.com/jetpack/androidx/versions
+
     const val android_core_ktx_version = "1.3.2"
     const val android_appcompat_version = "1.2.0"
     const val android_material_version = "1.2.1"
-    const val android_compose_version = "1.0.0-beta01"
+    const val android_compose_version = "1.0.0-beta06"
     const val android_lifecycle_version = "2.3.0-alpha06"
     const val android_activity_compose_version = "1.3.0-alpha03"
     const val android_compose_vm_version = "1.0.0-alpha02"
